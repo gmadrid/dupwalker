aa49fc1015d6914e9a8262d5ac0c98639d36d18c
George Madrid <gmadrid@gmail.com>
2023-03-16 11:51:24 -0400

    Moving the Status sender to "managed state" and added some test routes.

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,27 +1,46 @@
 use crate::engine::status_mgr::StatusMgrMsg;
 use crossbeam_channel::{Receiver, Sender};
-use rocket::request::{FromRequest, Outcome};
-use rocket::{routes, Request};
+use rocket::{routes, State};
 use std::path::PathBuf;
+use rocket::response::Redirect;
 
 mod file_walker;
 mod hasher;
 mod image_loader;
 mod status_mgr;
 
-fn first_or_default<T: Default>(mut i: impl Iterator<Item = T>) -> T {
-    i.next().unwrap_or_default()
+fn first_or_default<T: Default>(recv: Receiver<T>) -> T {
+    recv.iter().next().unwrap_or_default()
 }
 
-#[get("/world")]
-fn test_foo(status_sndr: StatusMsgSender) -> String {
-    first_or_default(status_sndr.test().iter())
+#[get("/")]
+fn root() -> Redirect {
+    Redirect::to(uri!(app_root()))
+}
+
+#[get("/app")]
+fn app_root(status_sndr: &State<Sender<StatusMgrMsg>>) -> String {
+    let (sndr, recv) = crossbeam_channel::bounded(1);
+    (*status_sndr).clone().send(StatusMgrMsg::TestMsg(sndr)).unwrap();
+    format!("ROOT: {}", first_or_default(recv))
+}
+
+#[get("/noop")]
+fn noop(status_sndr: &State<Sender<StatusMgrMsg>>) -> String {
+    (*status_sndr).clone().send(StatusMgrMsg::NoOp).unwrap();
+    "NoOp".to_string()
 }
 
 #[rocket::main]
 async fn start_rocket() -> Result<(), rocket::Error> {
+    // We pass this in a global since Rocket won't let me add parameters to this function.
+    // We 'take' the Sender so that we don't leave any unmanaged references to it.
+    let sndr = unsafe { STATUS_MSG_SENDER.take().unwrap() };
+
     let _r = rocket::build()
-        .mount("/", routes![test_foo])
+        .mount("/", routes![root, app_root])
+        .mount("/api", routes![noop])
+        .manage(sndr)
         .launch()
         .await?;
 
@@ -32,34 +51,11 @@ pub struct Engine;
 
 static mut STATUS_MSG_SENDER: Option<Sender<StatusMgrMsg>> = None;
 
-struct StatusMsgSender(Sender<StatusMgrMsg>);
-
-impl StatusMsgSender {
-    fn noop(&self) {
-        self.0.send(StatusMgrMsg::NoOp).unwrap();
-    }
-
-    fn test(&self) -> Receiver<String> {
-        let (sndr, recv) = crossbeam_channel::bounded(1);
-        self.0.send(StatusMgrMsg::TestMsg(sndr)).unwrap();
-        recv
-    }
-}
-
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for StatusMsgSender {
-    type Error = ();
-
-    async fn from_request(_: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        let sndr = unsafe { STATUS_MSG_SENDER.as_ref().cloned().unwrap() };
-        Outcome::Success(StatusMsgSender(sndr))
-    }
-}
-
 impl Engine {
     pub fn run(self, roots: &[PathBuf]) {
         let status_sndr = status_mgr::start();
         unsafe {
+            // AFAIK, we have no way to pass this to the Rocket builder func, so we use a global.
             STATUS_MSG_SENDER = Some(status_sndr.clone());
         }
 
